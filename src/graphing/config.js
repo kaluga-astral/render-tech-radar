ac1ea14cd3b250252f137f070b0fdfcb4b339682
Andrey Potyomkin <and_tem@icloud.com>
2023-10-06 13:13:29 +0300

    feat: Переименованы квадранты

--- src/graphing/config.js
+++ src/graphing/config.js
@@ -2,7 +2,7 @@ const quadrantSize = 512
 const quadrantGap = 32
 
 const getQuadrants = () => {
-  return JSON.parse(process.env.QUADRANTS || null) || ['Techniques', 'Platforms', 'Tools', 'Languages & Frameworks']
+  return JSON.parse(process.env.QUADRANTS || null) || ['Techniques', 'Infrastructure', 'Libs', 'Languages & Frameworks']
 }
 
 const getRings = () => {
